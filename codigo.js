032f48b40e4c94953ee6992b5d1ebbc64eee5e4a
jodaniel <133549697+Daniel0390@users.noreply.github.com>
2024-03-10 18:23:45 -0400

    impruving

---
+++ codigo.js
@@ -0,0 +1,31 @@
+
+
+
+
+let btntrad =document.getElementById("btn_trad");  
+let btnp92  =document.getElementById("btn_p92");
+let btnp65  =document.getElementById("btn_p65");
+let btnalum =document.getElementById("btn_alum");
+
+let cont1 =  document.getElementById("cont1");
+
+btntrad.onclick = cambio1;
+btnp92.onclick = cambio2;
+btnp65.onclick = cambio3;
+btnalum.onclick= cambio4;
+
+function cambio1 (){
+   cont1.style.backgroundColor='rgb(125, 118, 118)';
+}
+
+function cambio2 (){
+    cont1.style.backgroundColor='rgb(181, 157, 128)';
+ }
+
+ function cambio3 (){
+    cont1.style.backgroundColor='rgb(190, 129, 129)';
+ }
+
+ function cambio4 (){
+    cont1.style.backgroundColor='rgb(12, 67, 12)';
+ }
